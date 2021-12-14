194cda7676ee54630dc6f9eda29483107bf6c211
Chris Combs <ccombs@gmail.com>
2021-12-14 11:37:04 -0500

    clean up

--- 14/polymerize.py
+++ 14/polymerize.py
@@ -11,26 +11,16 @@ def get_atoms(pairs):
     for letter in letters:
         quant = 0
         for pair in pairs:
-            if pair[0]==letter:
+            if pair[0] == letter:
                 quant += pairs[pair]
-            # elif pair[0]==start:
-            #     quant += pairs[pair]
-            if pair[1]==letter:
+            if pair[1] == letter:
                 quant += pairs[pair]
-            # elif pair[1]==end:
-            #     quant += pairs[pair]
-            
-            # if letter in pair:
-            #     if ((start in pair) or (end in pair)) and (pairs[pair] > 0):
-            #         quant += 1
-            #         print(letter, quant)
-
-        quants[letter] = quant / 2
+        quants[letter] = int(quant / 2)
 
     return quants
 
-def parse_pairs(string):
-    returner = {}
+def parse_pairs(string, returner={}):
+
     # use magic tokens for start and end of sequence
     for pair in [ start + string[0], string[-1] + end ]:
         returner[pair] = 1
@@ -44,15 +34,16 @@ def parse_pairs(string):
 def polymerize(pairs, rules):
 
     newpairs = dict(pairs)
-    # print(pairs)
-    for pair in pairs:
-        if pair in rules:
-            quant = pairs[pair]
-            outcomes = rules[pair]
-            newpairs[pair] = newpairs.get(pair, 0) - quant
-            for outcome in outcomes:
-                # print(quant,"count of",pair,"becomes",outcome)
-                newpairs[outcome] = newpairs.get(outcome, 0) + quant
-            # print(newpairs)
+
+    for pair in [item for item in pairs.keys() if item in rules]:  # get intersection of lists
+
+        quant = pairs[pair]
+        outcomes = rules[pair]
+        newpairs[pair] = newpairs.get(pair, 0) - quant
+
+        for outcome in outcomes:
+            # print(quant,"count of",pair,"becomes",outcome)
+            newpairs[outcome] = newpairs.get(outcome, 0) + quant
+        # print(newpairs)
     
     return newpairs
